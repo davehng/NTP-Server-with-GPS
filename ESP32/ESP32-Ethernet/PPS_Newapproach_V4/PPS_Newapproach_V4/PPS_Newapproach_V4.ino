d093a703b0b5e35c47f853f6c47859a3a7081dbf
Dave Hng <dave.hng@gmail.com>
2026-01-05 22:58:36 +0800

    use a socket and bind to wait for an incoming NTP message

--- ESP32/ESP32-Ethernet/PPS_Newapproach_V4/PPS_Newapproach_V4/PPS_Newapproach_V4.ino
+++ ESP32/ESP32-Ethernet/PPS_Newapproach_V4/PPS_Newapproach_V4/PPS_Newapproach_V4.ino
@@ -1,12 +1,13 @@
 #include <ETH.h>
-#include <WiFiUdp.h>
 #include <TinyGPSPlus.h>
 #include <time.h>
 #include <lwip/def.h>
+#include <lwip/sockets.h> // Required for BSD Sockets
 #include <freertos/FreeRTOS.h>
 #include <freertos/task.h>
 
 // --- CONFIGURATION ---
+// Pins (WT32-ETH01)
 #define GPS_RX_PIN       15
 #define GPS_TX_PIN       4
 #define PPS_PIN          2
@@ -16,24 +17,35 @@
 #define ETH_MDIO_PIN     18
 #define ETH_TYPE         ETH_PHY_LAN8720
 #define ETH_CLK_MODE     ETH_CLOCK_GPIO0_IN
+
+// Settings
 #define GPS_BAUD         38400
 #define CORRECTION_US    1210
 #define NTP_EPOCH_OFFSET 2208988800UL
+#define NTP_PORT         123
 
 // --- OBJECTS & GLOBALS ---
 TinyGPSPlus gps;
-WiFiUDP udp;
 HardwareSerial gpsSerial(1);
 volatile uint32_t lastPPSMicros = 0;
 
 // NTP Packet Structure
 typedef struct {
-  uint8_t li_vn_mode; uint8_t stratum; uint8_t poll; uint8_t precision;
-  uint32_t rootDelay; uint32_t rootDispersion; uint32_t refId;
-  uint32_t refTm_s; uint32_t refTm_f;
-  uint32_t origTm_s; uint32_t origTm_f;
-  uint32_t rxTm_s; uint32_t rxTm_f;
-  uint32_t txTm_s; uint32_t txTm_f;
+  uint8_t li_vn_mode; 
+  uint8_t stratum; 
+  uint8_t poll; 
+  uint8_t precision;
+  uint32_t rootDelay; 
+  uint32_t rootDispersion; 
+  uint32_t refId;
+  uint32_t refTm_s; 
+  uint32_t refTm_f;
+  uint32_t origTm_s; 
+  uint32_t origTm_f;
+  uint32_t rxTm_s; 
+  uint32_t rxTm_f;
+  uint32_t txTm_s; 
+  uint32_t txTm_f;
 } ntp_packet_t;
 
 // --- INTERRUPT SERVICE ROUTINE ---
@@ -49,11 +61,10 @@ void initNetwork() {
   
   ETH.begin(ETH_TYPE, ETH_ADDR, ETH_MDC_PIN, ETH_MDIO_PIN, ETH_POWER_PIN, ETH_CLK_MODE);
   
-  // Minimal wait for IP (Blocking here is acceptable for network reliability)
+  // Minimal wait for IP (Blocking here is acceptable for startup reliability)
   while (ETH.localIP() == IPAddress(0,0,0,0)) { delay(500); }
   
   Serial.print("IP: "); Serial.println(ETH.localIP());
-  udp.begin(123);
 }
 
 // --- TASKS ---
@@ -108,30 +119,55 @@ void readGPSTime() {
   }
 }
 
-void handleNTPRequest() {
-  if (udp.parsePacket() >= sizeof(ntp_packet_t)) {
+// High Performance Blocking NTP Task
+void ntpTask(void *parameter) {
+  int sock = socket(AF_INET, SOCK_DGRAM, IPPROTO_IP);
+  if (sock < 0) { 
+    Serial.println("Socket failed"); 
+    vTaskDelete(NULL); 
+  }
+
+  struct sockaddr_in server_addr;
+  server_addr.sin_family = AF_INET;
+  server_addr.sin_port = htons(NTP_PORT);
+  server_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+
+  if (bind(sock, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0) {
+    Serial.println("Bind failed"); 
+    vTaskDelete(NULL);
+  }
+
+  while (1) {
     ntp_packet_t packet;
-    udp.read((uint8_t*)&packet, sizeof(ntp_packet_t));
+    struct sockaddr_in source_addr;
+    socklen_t addrLen = sizeof(source_addr);
 
-    struct timeval tv;
-    gettimeofday(&tv, NULL);
-    
-    // Convert to NTP Timestamp format
-    uint32_t s = htonl(tv.tv_sec + NTP_EPOCH_OFFSET);
-    uint32_t f = htonl((uint32_t)((double)tv.tv_usec * 4294.967296)); 
+    // RECVFROM: Blocks until data arrives (Zero Polling Jitter)
+    int len = recvfrom(sock, &packet, sizeof(packet), 0, (struct sockaddr *)&source_addr, &addrLen);
 
-    // Construct Response
-    packet.li_vn_mode = 0x24; // Server, Stratum 1
-    packet.stratum = 1;
-    packet.refId = htonl(0x47505300); // "GPS"
-    packet.origTm_s = packet.txTm_s; // Copy client transmit time
-    packet.origTm_f = packet.txTm_f;
-    packet.rxTm_s = s; packet.rxTm_f = f;
-    packet.txTm_s = s; packet.txTm_f = f;
+    if (len == sizeof(ntp_packet_t)) {
+      struct timeval tv;
+      gettimeofday(&tv, NULL);
+      
+      // Convert to NTP Format
+      uint32_t s = htonl(tv.tv_sec + NTP_EPOCH_OFFSET);
+      uint32_t f = htonl((uint32_t)((double)tv.tv_usec * 4294.967296)); 
 
-    udp.beginPacket(udp.remoteIP(), udp.remotePort());
-    udp.write((uint8_t*)&packet, sizeof(ntp_packet_t));
-    udp.endPacket();
+      // Fill Response
+      packet.li_vn_mode = 0x24; // Server, Stratum 1
+      packet.stratum = 1;
+      packet.refId = htonl(0x47505300); // "GPS"
+      
+      // Originate = Client's Transmit Time
+      packet.origTm_s = packet.txTm_s; 
+      packet.origTm_f = packet.txTm_f;
+      
+      // RX and TX times are effectively "now" for this simplified server
+      packet.rxTm_s = s; packet.rxTm_f = f;
+      packet.txTm_s = s; packet.txTm_f = f;
+
+      sendto(sock, &packet, sizeof(packet), 0, (struct sockaddr *)&source_addr, addrLen);
+    }
   }
 }
 
@@ -143,26 +179,25 @@ void gpsTask(void *p) {
   } 
 }
 
-void ntpTask(void *p) { 
-  while(1) { 
-    handleNTPRequest(); 
-    vTaskDelay(pdMS_TO_TICKS(1)); 
-  } 
-}
-
 void setup() {
+  // 1. Lock CPU to max freq to prevent power-saving latency
+  setCpuFrequencyMhz(240);
+  
   Serial.begin(115200);
   
-  // Hardware Init (Non-blocking)
+  // 2. Hardware Init
   gpsSerial.begin(GPS_BAUD, SERIAL_8N1, GPS_RX_PIN, GPS_TX_PIN);
   pinMode(PPS_PIN, INPUT);
   attachInterrupt(digitalPinToInterrupt(PPS_PIN), PPS_ISR, RISING);
   
   initNetwork();
 
-  // Tasks
+  // 3. Start Tasks
+  // GPS Maintenance: Priority 2 (Standard)
   xTaskCreatePinnedToCore(gpsTask, "GPS", 4096, NULL, 2, NULL, 1);
-  xTaskCreatePinnedToCore(ntpTask, "NTP", 4096, NULL, 3, NULL, 0);
+  
+  // NTP Server: Priority 20 (High Real-Time Priority)
+  xTaskCreatePinnedToCore(ntpTask, "NTP", 4096, NULL, 20, NULL, 1);
 }
 
 void loop() { 
